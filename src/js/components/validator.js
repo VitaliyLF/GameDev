3cac1973a2b2d6eb4f2f6ba1b876c854788b828f
Vitalii Stepnov <97784759+Vetosy@users.noreply.github.com>
2023-06-14 03:28:19 +0400

    added validator and modal

---
+++ src/js/components/validator.js
@@ -0,0 +1,72 @@
+import {
+  validateForms
+} from '../functions/validate-forms';
+
+const rules1 = [{
+    ruleSelector: '.input-name',
+    rules: [{
+      rule: 'required',
+      value: true,
+      errorMessage: 'Заполните имя!'
+    }]
+  },
+  {
+    ruleSelector: '.input-tel',
+    tel: true,
+    telError: 'Введите корректный телефон',
+    rules: [{
+      rule: 'required',
+      value: true,
+      errorMessage: 'Заполните телефон!'
+    }]
+  }
+];
+
+const rules2 = [{
+    ruleSelector: '.input-name',
+    rules: [{
+      rule: 'required',
+      value: true,
+      errorMessage: 'Заполните имя!'
+    }]
+  },
+  {
+    ruleSelector: '.input-tel',
+    tel: true,
+    telError: 'Введите корректный телефон',
+    rules: [{
+      rule: 'required',
+      value: true,
+      errorMessage: 'Заполните телефон!'
+    }]
+  }
+];
+
+const rules3 = [{
+    ruleSelector: '.input-name',
+    rules: [{
+      rule: 'required',
+      value: true,
+      errorMessage: 'Заполните имя!'
+    }]
+  },
+  {
+    ruleSelector: '.input-tel',
+    tel: true,
+    telError: 'Введите корректный телефон',
+    rules: [{
+      rule: 'required',
+      value: true,
+      errorMessage: 'Заполните телефон!'
+    }]
+  }
+];
+
+
+const afterForm = () => {
+  console.log('Произошла отправка, тут можно писать любые действия');
+};
+
+validateForms('.steps__apply', rules1, afterForm);
+validateForms('.stage__apply', rules2, afterForm);
+validateForms('.site-modal__apply', rules3, afterForm);
